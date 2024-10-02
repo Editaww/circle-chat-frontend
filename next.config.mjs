51bfa7b492ce225785f1e85044a7ca844c09da0b
Editaww <151859796+Editaww@users.noreply.github.com>
2024-10-02 22:49:22 +0300

    changed btn style

--- next.config.mjs
+++ next.config.mjs
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    SERVER_URL: "http://localhost:3000",
-    // SERVER_URL: "https://travel-spot-api.onrender.com",
+    // SERVER_URL: "http://localhost:3000",
+    SERVER_URL: "https://travel-spot-api.onrender.com",
     JWT_KEY: "question_app_jwt",
   },
 };
